1eebaf3df1a267ed6f66a71e8e8f15c08b51f14e
Nadiia Damrina <nadiia.damrina@priceline.com>
2020-05-27 14:59:31 -0700

    initial routing

--- wbdv-s1-2020-jh-nd-client-react/src/components/TutorMeComponent.js
+++ wbdv-s1-2020-jh-nd-client-react/src/components/TutorMeComponent.js
@@ -1,2 +1,31 @@
 import React from 'react';
 import {BrowserRouter, Route} from "react-router-dom";
+import HomeComponent from "./HomeComponent";
+import SearchComponent from "./SearchComponent";
+import DetailsComponent from "./DetailsComponent";
+import ResultsComponent from "./ResultsComponent";
+
+class TutorMeComponent extends React.Component {
+    render() {
+        return (
+            <BrowserRouter>
+                <div>
+                    <Route path="/"
+                           exact
+                           component={HomeComponent}/>
+                    <Route path="/search"
+                           exact
+                           component={SearchComponent}/>
+                    <Route path="/results"
+                           exact
+                           component={ResultsComponent}/>
+                    <Route path="/details/"
+                           exact
+                           component={DetailsComponent}/>
+                </div>
+            </BrowserRouter>
+        )
+    }
+}
+
+export default TutorMeComponent
