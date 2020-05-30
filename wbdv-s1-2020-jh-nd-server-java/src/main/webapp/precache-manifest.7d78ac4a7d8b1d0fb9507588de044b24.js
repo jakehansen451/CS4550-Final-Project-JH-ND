b2886e2d11038cc907f18fe11119827ca081d35e
Nadiia Damrina <nadiia.damrina@priceline.com>
2020-05-29 19:20:05 -0700

    add front end

---
+++ wbdv-s1-2020-jh-nd-server-java/src/main/webapp/precache-manifest.7d78ac4a7d8b1d0fb9507588de044b24.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c51c67ed43403e75d9702983a1b7a733",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b2bc4a7394377bef70b2",
+    "url": "/static/css/2.ddfacf9b.chunk.css"
+  },
+  {
+    "revision": "9a6d0b58fb96351a8a00",
+    "url": "/static/css/main.97ae51b2.chunk.css"
+  },
+  {
+    "revision": "b2bc4a7394377bef70b2",
+    "url": "/static/js/2.9a3399e7.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.9a3399e7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9a6d0b58fb96351a8a00",
+    "url": "/static/js/main.5250f9a6.chunk.js"
+  },
+  {
+    "revision": "ea822498b86a226cb21d",
+    "url": "/static/js/runtime-main.c8aa0f64.js"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/static/media/fontawesome-webfont.fee66e71.woff"
+  }
+]);
