f04291b5f12678205e112026e9d3d20fc87c1dbb
Jake Hansen <hansen.j@husky.neu.edu>
2020-06-14 23:22:30 -0400

    Adds admin model

--- wbdv-s1-2020-jh-nd-server-java/src/main/java/com/example/myapp/models/Admin.java
+++ wbdv-s1-2020-jh-nd-server-java/src/main/java/com/example/myapp/models/Admin.java
@@ -1,5 +1,34 @@
 package com.example.myapp.models;
 
-public class Admin {
+import java.util.ArrayList;
+import java.util.List;
 
+public class Admin extends User {
+  private final ArrayList<Course> coursesTaught;
+  private final ArrayList<Event> officeHours;
+
+  public Admin(List<Course> coursesTaught, List<Event> officeHours) {
+    this.coursesTaught = new ArrayList<>();
+    this.coursesTaught.addAll(coursesTaught);
+    this.officeHours = new ArrayList<>();
+    this.officeHours.addAll(officeHours);
+  }
+
+  public List<Course> getCoursesTaught() {
+    return coursesTaught;
+  }
+
+  public void setCoursesTaught(List<Course> coursesTaught) {
+    this.coursesTaught.clear();
+    this.coursesTaught.addAll(coursesTaught);
+  }
+
+  public List<Event> getOfficeHours() {
+    return officeHours;
+  }
+
+  public void setOfficeHours(ArrayList<Event> officeHours) {
+    this.officeHours.clear();
+    this.officeHours.addAll(officeHours);
+  }
 }
