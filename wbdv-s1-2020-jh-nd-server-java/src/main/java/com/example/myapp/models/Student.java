b595bfbfced20e099137a8cc50c69fde6d758758
Jake Hansen <hansen.j@husky.neu.edu>
2020-06-14 23:23:21 -0400

    Adds student model

--- wbdv-s1-2020-jh-nd-server-java/src/main/java/com/example/myapp/models/Student.java
+++ wbdv-s1-2020-jh-nd-server-java/src/main/java/com/example/myapp/models/Student.java
@@ -1,5 +1,22 @@
 package com.example.myapp.models;
 
-public class Student {
+import java.util.ArrayList;
+import java.util.List;
 
+public class Student extends User {
+  private final ArrayList<Course> coursesEnrolled;
+
+  public Student(List<Course> coursesEnrolled) {
+    this.coursesEnrolled = new ArrayList<>();
+    this.coursesEnrolled.addAll(coursesEnrolled);
+  }
+
+  public List<Course> getCoursesEnrolled() {
+    return coursesEnrolled;
+  }
+
+  public void setCoursesEnrolled(List<Course> coursesEnrolled) {
+    this.coursesEnrolled.clear();
+    this.coursesEnrolled.addAll(coursesEnrolled);
+  }
 }
